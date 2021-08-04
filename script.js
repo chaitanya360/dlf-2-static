1d1fbdb96823b3825c90c14e3a14bffab49e788c
Chaitanya360 <chaitanyajadhav360@gmail.com>
2021-08-04 18:10:18 +0530

    added new btn

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let elevationBtn = document.getElementById("elevation_btn");
 let isoBtn = document.getElementById("iso_btn");
 let virtualBtn = document.getElementById("virtual_btn");
+let onSiteBtn = document.getElementById("on_site_btn");
 let iframe = document.getElementById("6b0ec302-ba89-4dbf-99d5-6fcfe3d661b2");
 let badge = document.getElementById("badge_container");
 
@@ -8,20 +9,26 @@ let btnBg = "#ffffff";
 let btnColor = "#2c3233";
 
 const elevationURL =
-  "https://www.vectary.com/viewer/v1/?model=7d59a429-163b-4b0b-b2c7-48f108edc7bf&env=studio3";
-  
+  "https://www.vectary.com/viewer/v1/?model=31f82e89-7208-4add-8ebe-6cb0b1d858b6&env=studio3";
 
 const isoURL =
   "https://www.vectary.com/viewer/v1/?model=38c02e98-5248-42f1-96af-bebd88cc0e8b&env=studio3";
 const virtualURL = "https://makevr.pro/Dlf%20furished%20tour/";
 
+const onSiteURL =
+  "https://www.vectary.com/viewer/v1/?model=70dd7f62-abb7-418d-ae8c-a30982817756&env=studio3";
+
 const resetBtnStyle = () => {
   elevationBtn.style.backgroundColor = btnBg;
   isoBtn.style.backgroundColor = btnBg;
+  onSiteBtn.style.backgroundColor = btnBg;
   virtualBtn.style.backgroundColor = btnBg;
+
   elevationBtn.style.color = btnColor;
   isoBtn.style.color = btnColor;
   virtualBtn.style.color = btnColor;
+  onSiteBtn.style.color = btnColor;
+
   badge.style.display = "flex";
   badge.style.visibility = "hidden";
 };
@@ -42,6 +49,9 @@ const handleBtnClick = (e) => {
       src = virtualURL;
       badge.style.display = "none";
       break;
+    case "on_site_btn":
+      src = onSiteURL;
+      break;
   }
   iframe.setAttribute("src", src);
 };
@@ -51,5 +61,6 @@ handleBtnClick({ target: { id: "elevation_btn" } });
 elevationBtn.onclick = handleBtnClick;
 isoBtn.onclick = handleBtnClick;
 virtualBtn.onclick = handleBtnClick;
+onSiteBtn.onclick = handleBtnClick;
 
 iframe.onload = () => (badge.style.visibility = "visible");
